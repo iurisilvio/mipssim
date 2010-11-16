abd1f2eb8c7b044cd7616b1cadc078bd9b96bf94
iurisilvio <iurisilvio@sereianb.(none)>
2010-11-16 21:15:11 -0200

    Testando commit.

--- interpreter.py
+++ interpreter.py
@@ -15,64 +15,126 @@ LABEL = "label"
 
 _instructions = [ADD, ADDI, BEQ, BLE, BNE, JMP, LW, MUL, NOP, SUB, SW]
 
+RE_NOP = re.compile(r"^(?P<instruction>nop)$")
+RE_JMP = re.compile(r"^(?P<instruction>jmp) (?P<label>\w+)$")
+RE_LOAD_STORE = re.compile(r"^(?P<instruction>\w+) (?P<rt>R\d+),(?P<immediate>\d+)\((?P<rs>R\d+)\)$")
+RE_WITH_IMMEDIATE = re.compile(r"^(?P<instruction>\w+) (?P<rt>R\d+),(?P<rs>R\d+),(?P<immediate>\d+)$")
+RE_WITH_LABEL = re.compile(r"^(?P<instruction>\w+) (?P<rs>R\d+),(?P<rt>R\d+),(?P<label>\w+)$")
+RE_WITH_REGISTER = re.compile(r"^(?P<instruction>\w+) (?P<rd>R\d+),(?P<rs>R\d+),(?P<rt>R\d+)$")
+RE_LABEL = re.compile(r"^(?P<label>\w+):$")
+
+map_re = {NOP:RE_NOP,
+          JMP:RE_JMP,
+          LW:RE_LOAD_STORE,
+          SW:RE_LOAD_STORE,
+          ADDI:RE_WITH_IMMEDIATE,
+          BEQ:RE_WITH_LABEL,
+          BLE:RE_WITH_LABEL,
+          BNE:RE_WITH_LABEL,
+          ADD:RE_WITH_REGISTER,
+          MUL:RE_WITH_REGISTER,
+          SUB:RE_WITH_REGISTER}
+
+map_opcode = {ADD:"000000",
+              ADDI:"001000",
+              BEQ:"000101",
+              BLE:"000111",
+              BNE:"000100",
+              JMP:"000010",
+              LW:"100011",
+              MUL:"000000",
+              NOP:"000000",
+              SUB:"000000",
+              SW:"101011"}
+              
+map_funct = {ADD:"100000",
+             MUL:"011000",
+             NOP:"000000",
+             SUB:"100010"}
+
 
 class Interpreter(object):
 
+
     def __init__(self, text=None):
         splitted_text = text.split("\n") if text else []
-        self.text_instructions = [t.strip() for t in splitted_text]
+        self.text_instructions = [t.strip() for t in splitted_text if t.strip()]
         self.bytecode_instructions = []
         self.labels = {}
         
-    def _parse(self, line):
+    def _parse_line(self, line):
         data = line.split(" ", 1)
         instruction = data[0]
         tokens_re = None
+        is_label = False
         
         if instruction in _instructions:
-            if instruction == NOP:
-                tokens_re = re.compile("^(?P<instruction>nop)$")
-            elif instruction == JMP:
-                tokens_re = re.compile("^(?P<instruction>jmp) (?P<label>\w+)$")
-            elif instruction in [LW, SW]:
-                tokens_re = re.compile("^(?P<instruction>\w+) (?P<rt>R\d+),(?P<immediate>\d+)\((?<rs>R\d+)\)$")
-            elif instruction in [ADDI, BEQ, BLE, BNE]:
-                tokens_re = re.compile("^(?P<instruction>\w+) (?P<rt>R\d+),(?P<rs>R\d+),(?P<immediate>\d+)$")
-            elif instruction in [ADD, MUL, SUB]:
-                tokens_re = re.compile("^(?P<instruction>\w+) (?P<rd>R\d+),(?P<rs>R\d+),(?P<rt>R\d+)$")
+            tokens_re = map_re[instruction]
         else:
             # label
-            tokens_re = re.compile("^(?P<label>\w+):$")
+            tokens_re = RE_LABEL
+            is_label = True
         
-        if tokens_re:
-            tokens = tokens_re.match(line).groupdict()
+        matches = tokens_re.match(line)
+        
+        if matches:
+            tokens = matches.groupdict()
             if tokens:
-                if tokens.get("label"):
+                if is_label:
                     tokens["instruction"] = "label"
                 return tokens
 
-        raise Exception()
+        raise Exception(line)
         
-    def _compile_line(self, tokens, labels={}):
+    def _compile_line(self, tokens):
         instruction = tokens["instruction"]
         
         if instruction in _instructions:
+            opcode = map_opcode[instruction]
+        
             if instruction in  NOP:
-                bytecode = "00000000000000000000000000000000"
+                bytecode = self._to_bin(0, 32)
             if instruction == JMP:
-                pass
-        else:
-            # label
-            bytecode = ""
-            
-        return bytecode
+                target_address = self._to_bin(self.labels[tokens.get("label")], 26)
+                bytecode = "%s%s" % (opcode, target_address)
+            if instruction in (ADDI, LW, SW):
+                rs = self._register_to_bin(tokens.get("rs"))
+                rt = self._register_to_bin(tokens.get("rt"))
+                immediate = self._to_bin(tokens.get("immediate"), 16)
+                bytecode = "%s%s%s%s" % (opcode, rs, rt, immediate)
+
+            if instruction in (BEQ, BLE, BNE):
+                rs = self._register_to_bin(tokens.get("rs"))
+                rt = self._register_to_bin(tokens.get("rt"))
+                immediate = self._to_bin(self.labels[tokens.get("label")], 16)
+                bytecode = "%s%s%s%s" % (opcode, rs, rt, immediate)
+
+            if instruction in (ADD, MUL, SUB):
+                rs = self._register_to_bin(tokens.get("rs"))
+                rt = self._register_to_bin(tokens.get("rt"))
+                rd = self._register_to_bin(tokens.get("rd"))
+                shamt = "00000"
+                funct = map_funct[instruction]
+                bytecode = "%s%s%s%s%s%s" % (opcode, rs, rt, rd, shamt, funct)
+                
+            return bytecode
+
+    def _register_to_bin(self, register):
+        return self._to_bin(int(register.strip("R")), 5) 
+        
+    def _to_bin(self, number, length=0):
+        """
+        Convert `number` to binary, appending zeros to left to return a string with length.
+        """
+        zeros = "0" * length
+        binary = bin(int(number))[2:]
+        return ("%s%s" % (zeros, binary))[-length:]
         
     def parse(self):
         result = []
         pc = 0
-
         for instruction in self.text_instructions:
-            tokens = self._parse(instruction)
+            tokens = self._parse_line(instruction)
             if tokens["instruction"] == LABEL:
                 self.labels[tokens["label"]] = pc
             else:
@@ -85,16 +147,30 @@ class Interpreter(object):
         parsed_instructions = self.parse()
         self.bytecode_instructions = []
         pc = 0
-        
+                
         for instruction in parsed_instructions:
             pc += 4
             self.bytecode_instructions.append(self._compile_line(instruction))
             
     def __str__(self):
         result = ""
-        for instruction, bytecode in zip(self.instructions, self.bytecode):
+        for instruction, bytecode in zip(self.text_instructions, self.bytecode_instructions):
             result += "%s ; %s\n" % (bytecode, instruction)
             
         return result
         
+        
+    def get_bytecode(self):
+        return "\n".join(self.bytecode_instructions)
+        
+    bytecode = property(get_bytecode)
+    
+    
+if __name__ == "__main__":
+    import sys
+    
+    text = file(sys.argv[1]).read()
+    interpreter = Interpreter(text)
+    interpreter.compile()
+    print interpreter
 
--- test/test_interpreter.py
+++ test/test_interpreter.py
@@ -3,17 +3,49 @@ import unittest
 from interpreter import Interpreter
 
 class TestCompile(unittest.TestCase):
-    def test_nop(self):
-        code = "nop"
+
+    def test_add(self):
+        code = "add R9,R1,R7"
         interpreter = Interpreter(code)
         interpreter.compile()
-        self.assertEqual(interpreter.bytecode_instructions[0], "00000000000000000000000000000000")
+        self.assertEqual(len(interpreter.bytecode_instructions), 1)
+        self.assertEqual(interpreter.bytecode_instructions[0], "00000000001001110100100000100000")
         
-    def test_label(self):
-        code = "label:"
+    def test_addi(self):
+        code = "addi R6,R5,1"
         interpreter = Interpreter(code)
         interpreter.compile()
-        self.assertEqual(len(interpreter.bytecode_instructions), 0)
+        self.assertEqual(len(interpreter.bytecode_instructions), 1)
+        self.assertEqual(interpreter.bytecode_instructions[0], "00100000101001100000000000000001")
+
+    def test_beq(self):
+        code = """nop
+                  LOOP:
+                  nop
+                  beq R1,R2,LOOP"""
+        interpreter = Interpreter(code)
+        interpreter.compile()
+        self.assertEqual(len(interpreter.bytecode_instructions), 3)
+        self.assertEqual(interpreter.bytecode_instructions[2], "00010100001000100000000000000100")
+        
+    def test_ble(self):
+        code = """nop
+                  LOOP:
+                  nop
+                  ble R1,R2,LOOP"""
+        interpreter = Interpreter(code)
+        interpreter.compile()
+        self.assertEqual(len(interpreter.bytecode_instructions), 3)
+        self.assertEqual(interpreter.bytecode_instructions[2], "00011100001000100000000000000100")
+        
+    def test_bne(self):
+        code = """nop
+                  LOOP:
+                  bne R1,R2,LOOP"""
+        interpreter = Interpreter(code)
+        interpreter.compile()
+        self.assertEqual(len(interpreter.bytecode_instructions), 2)
+        self.assertEqual(interpreter.bytecode_instructions[1], "00010000001000100000000000000100")
         
     def test_jmp_back(self):
         code = """label:
@@ -30,8 +62,80 @@ class TestCompile(unittest.TestCase):
         interpreter = Interpreter(code)
         interpreter.compile()
         self.assertEqual(len(interpreter.bytecode_instructions), 1)
-        self.assertEqual(interpreter.bytecode_instructions[0], "00001000000000000000000000000000")
+        self.assertEqual(interpreter.bytecode_instructions[0], "00001000000000000000000000000100")
         self.assertEqual(interpreter.labels.get("label"), 4)
+
+    def test_lw(self):
+        code = "lw R1,24(R0)"
+        interpreter = Interpreter(code)
+        interpreter.compile()
+        self.assertEqual(interpreter.bytecode_instructions[0], "10001100000000010000000000011000")
+        
+    def test_mul(self):
+        code = "mul R9,R1,R7"
+        interpreter = Interpreter(code)
+        interpreter.compile()
+        self.assertEqual(len(interpreter.bytecode_instructions), 1)
+        self.assertEqual(interpreter.bytecode_instructions[0], "00000000001001110100100000011000")
+
+    def test_nop(self):
+        code = "nop"
+        interpreter = Interpreter(code)
+        interpreter.compile()
+        self.assertEqual(interpreter.bytecode_instructions[0], "00000000000000000000000000000000")
+        
+    def test_sub(self):
+        code = "sub R9,R1,R7"
+        interpreter = Interpreter(code)
+        interpreter.compile()
+        self.assertEqual(len(interpreter.bytecode_instructions), 1)
+        self.assertEqual(interpreter.bytecode_instructions[0], "00000000001001110100100000100010")
+    
+    def test_sw(self):
+        code = "sw R1,24(R0)"
+        interpreter = Interpreter(code)
+        interpreter.compile()
+        self.assertEqual(interpreter.bytecode_instructions[0], "10101100000000010000000000011000")
+
+    def test_label(self):
+        code = "label:"
+        interpreter = Interpreter(code)
+        interpreter.compile()
+        self.assertEqual(len(interpreter.bytecode_instructions), 0)
+
+
+class TestInterpreter(unittest.TestCase):
+    def test_example(self):
+        example = """addi R10,R0,100
+                     sw R0,24(R0)
+                     sw R0,28(R0)
+                     lw R6,28(R0)
+                     mul R7,R6,R6
+                     LOOP:
+                     lw R1,24(R0)
+                     add R9,R1,R7
+                     sw R9,24(R0)
+                     addi R6,R6,1
+                     sw R6,28(R0)
+                     ble R6,R10,LOOP"""
+
+        bytecode = ["00100000000010100000000001100100",
+                    "10101100000000000000000000011000",
+                    "10101100000000000000000000011100",
+                    "10001100000001100000000000011100",
+                    "00000000110001100011100000011000",
+                    "10001100000000010000000000011000",
+                    "00000000001001110100100000100000",
+                    "10101100000010010000000000011000",
+                    "00100000110001100000000000000001",
+                    "10101100000001100000000000011100",
+                    "00011100110010100000000000010100"]
+        interpreter = Interpreter(example)
+        interpreter.compile()
+
+        for compiled, correct in zip(interpreter.bytecode_instructions, bytecode):
+            self.assertEqual(compiled, correct)
+        
 
 
     
